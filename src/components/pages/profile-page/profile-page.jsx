932835fd58810870b8e5f62a5bb418d66430875e
AHTOH2001 <Antonlapytko@gmail.com>
2023-05-09 11:21:31 +0200

    Added button add patient

--- src/components/pages/profile-page/profile-page.jsx
+++ src/components/pages/profile-page/profile-page.jsx
@@ -1,14 +1,15 @@
-import { Layout, Select } from 'antd'
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Route, Switch } from 'react-router-dom'
-import { setCurrentPatient } from '../../../redux/patient/patient.actions'
-import Header from '../../header/header'
-import Sider from '../../sider/sider'
-import Buy from './profile-contents/buy'
-import Home from './profile-contents/home'
-import Settings from './profile-contents/settings'
-import './profile-page.css'
+import { PlusOutlined } from '@ant-design/icons';
+import { Button, Layout, Select } from 'antd';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import { setCurrentPatient } from '../../../redux/patient/patient.actions';
+import Header from '../../header/header';
+import Sider from '../../sider/sider';
+import Buy from './profile-contents/buy';
+import Home from './profile-contents/home';
+import Settings from './profile-contents/settings';
+import './profile-page.css';
 
 const selectCurrentUser = (state) => state.user.currentUser
 
@@ -23,29 +24,47 @@ const ProfilePage = () => {
         dispatch(setCurrentPatient(patient))
     }
 
+    const getHeaderContent = () => {
+        let header_content = []
+        if (currentUser.guardian) {
+            header_content = [(
+                <Select
+                    placeholder="Select a patient"
+                    bordered={false}
+                    key={1}
+                    style={{ color: 'white' }}
+                    onChange={onSelectChange}
+                    options={currentUser.guardian.connected_patients.map(
+                        (patient) => {
+                            return {
+                                value: patient.id,
+                                label: `${patient.relationship}: ${patient.first_name} ${patient.last_name}`,
+                            }
+                        }
+                    )}
+                />
+            ),
+            (
+                <Button
+                    type="text"
+                    htmlType="submit"
+                    key={2}
+                    style={{ color: 'white' }}
+                    icon={<PlusOutlined />}
+                >
+                    Add patient
+                </Button>
+            ),
+            ]
+        }
+        return header_content
+    }
+
     return (
         <Layout style={{ minHeight: '100vh' }} className="profile">
             <Header
                 header_link={'/profile'}
-                content={[
-                    currentUser.guardian ? (
-                        <Select
-                            placeholder="Select a patient"
-                            bordered={false}
-                            key={1}
-                            style={{ color: 'white' }}
-                            onChange={onSelectChange}
-                            options={currentUser.guardian.connected_patients.map(
-                                (patient) => {
-                                    return {
-                                        value: patient.id,
-                                        label: `${patient.relationship}: ${patient.first_name} ${patient.last_name}`,
-                                    }
-                                }
-                            )}
-                        />
-                    ) : [],
-                ]}
+                content={getHeaderContent()}
             />
             <Layout style={{ minHeight: '100vh' }} className="profile">
                 <Sider />
