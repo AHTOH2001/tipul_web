fbe01f1677113389878571248cd207787a1c7f8f
AHTOH2001 <Antonlapytko@gmail.com>
2023-05-10 10:51:38 +0300

    Started pills

--- src/components/pages/profile-page/profile-contents/pills.jsx
+++ src/components/pages/profile-page/profile-contents/pills.jsx
@@ -1,6 +1,7 @@
 import { Col, Divider, Row, Typography } from 'antd'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { useSelector } from 'react-redux'
+import { SmartRequest } from '../../../../utils/utils'
 import ChangePassword from '../../../forms/change-password/change-password'
 import PatchProfile from '../../../forms/patch-profile/patch-profile'
 import UpdateGuardianForm from '../../../forms/update-guardian-form/update-guardian-form'
@@ -12,9 +13,21 @@ const headerTextStyle = {
 }
 
 const selectCurrentUser = (state) => state.user.currentUser
+const selectCurrentPatient = (state) => state.patient.currentPatient
+
 
 const Pills = () => {
     const currentUser = useSelector(selectCurrentUser)
+    const currentPatient = useSelector(selectCurrentPatient)
+    const [medicine, setMedicine] = useState([])
+
+    useEffect(() => {
+        SmartRequest.get('medicine/cure/').then((resp) => {
+            console.log('here', resp.data)
+            setMedicine(resp.data)
+        })
+    }, [currentPatient])
+
     return (
         <div>
             Pills
