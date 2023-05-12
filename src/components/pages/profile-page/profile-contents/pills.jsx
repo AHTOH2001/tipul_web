85ecc89b400048ce9b6516a7ddd12e17d58e9385
AHTOH2001 <Antonlapytko@gmail.com>
2023-05-12 08:35:30 +0300

    Pills list page

--- src/components/pages/profile-page/profile-contents/pills.jsx
+++ src/components/pages/profile-page/profile-contents/pills.jsx
@@ -1,38 +1,65 @@
-import { Col, Divider, Row, Typography } from 'antd'
-import React, {useEffect, useState} from 'react'
+import { EditOutlined } from '@ant-design/icons'
+import { Card, Col, Row } from 'antd'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { SmartRequest } from '../../../../utils/utils'
-import ChangePassword from '../../../forms/change-password/change-password'
-import PatchProfile from '../../../forms/patch-profile/patch-profile'
-import UpdateGuardianForm from '../../../forms/update-guardian-form/update-guardian-form'
-import PreferencesForm from '../../../forms/preferences-form/preferences-form'
-
-const headerTextStyle = {
-    fontSize: 40,
-    textAlign: 'center',
-}
 
 const selectCurrentUser = (state) => state.user.currentUser
 const selectCurrentPatient = (state) => state.patient.currentPatient
 
-
+// {
+//     "id": 42,
+//     "schedule": {
+//         "id": 46,
+//         "timesheet": [
+//             {
+//                 "id": 63,
+//                 "time": "09:10:00"
+//             }
+//         ],
+//         "cycle_start": "2023-02-11",
+//         "cycle_end": "2023-02-11",
+//         "frequency": 0
+//     },
+//     "title": "Новый медикамент",
+//     "dose": 1,
+//     "dose_type": "pcs",
+//     "type": "injection",
+//     "strict_status": false,
+//     "food": "Before meals",
+//     "patient": 11
+// }
 const Pills = () => {
     const currentUser = useSelector(selectCurrentUser)
     const currentPatient = useSelector(selectCurrentPatient)
-    const [medicine, setMedicine] = useState([])
+    const [medicines, setMedicines] = useState([])
 
     useEffect(() => {
         SmartRequest.get('medicine/cure/').then((resp) => {
             console.log('here', resp.data)
-            setMedicine(resp.data)
+            setMedicines(resp.data)
         })
     }, [currentPatient])
 
     return (
-        <div>
-            Pills
-                    
-        </div>
+        <Row justify="space-evenly" gutter={[2, 16]}>
+            {medicines.map(med => (
+                <Col md={12} xl={8} xxl={6} key={med.id}>
+                    <Card title={med.title}
+                        extra={
+                            <Link to={`/profile/pills/${med.id}`}>
+                                <EditOutlined />
+                            </Link>
+                        }
+                        style={{ width: 300 }}>
+                        <p>Тип: {med.type}</p>
+                        <p>Когда принимать: {med.food}</p>
+                        <p>Сколько принимать: {med.dose} {med.dose_type}</p>
+                    </Card>
+                </Col>
+            ))}
+        </Row>
     )
 }
 
