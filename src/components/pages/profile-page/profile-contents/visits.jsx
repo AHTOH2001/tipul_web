cf23c15044afe1ba8637466e648e39c3b1ac369d
AHTOH2001 <Antonlapytko@gmail.com>
2023-05-15 09:03:22 +0300

    Delete create list visit

--- src/components/pages/profile-page/profile-contents/visits.jsx
+++ src/components/pages/profile-page/profile-contents/visits.jsx
@@ -17,18 +17,21 @@ const selectCurrentPatient = (state) => state.patient.currentPatient
 const Visits = () => {
     const currentPatient = useSelector(selectCurrentPatient)
     const [visits, setVisits] = useState([])
+    const [docs, setDocs] = useState([])
     const history = useHistory()
 
     useEffect(() => {
         SmartRequest.get('managment/doctor/').then((resp) => {
             let received_doctors = resp.data
-            if (!received_doctors) {
+            setDocs(received_doctors)
+            if (received_doctors.length == 0) {
                 message.warning('Before creating visits you need to create doctors')
-                history.push('/profile/doctors/')
+                history.push('/profile/docs')
             }
             SmartRequest.get('managment/doctorvisit/').then((resp) => {
                 setVisits(resp.data.map(raw_visit => {
                     return {
+                        'id': raw_visit['id'],
                         'date': moment(raw_visit['date'], 'YYYY-MM-DDTHH:mm:ss'),
                         'doctor': received_doctors.find(doc => doc.id == raw_visit['doctor'].id)
                     }
@@ -39,19 +42,28 @@ const Visits = () => {
     }, [currentPatient])
 
     const createDefaultVisit = () => {
-        SmartRequest.post('managment/doctor/', {
-            'first_name': 'Новый',
-            'last_name': 'Доктор',
-            'specialty': 'therapist',
+        SmartRequest.post('managment/doctorvisit/', {
+            'date': moment().add(1, 'weeks').format('YYYY-MM-DDTHH:mm:ss'),
+            'doctor': docs[docs.length - 1].id,
         }).then(resp => {
-            setVisits([...visits, resp.data])
+            let raw_visit = resp.data
+            console.log({
+                'id': raw_visit['id'],
+                'date': moment(raw_visit['date'], 'YYYY-MM-DDTHH:mm:ss'),
+                'doctor': docs[docs.length - 1]
+            })
+            setVisits([...visits, {
+                'id': raw_visit['id'],
+                'date': moment(raw_visit['date'], 'YYYY-MM-DDTHH:mm:ss'),
+                'doctor': docs[docs.length - 1]
+            }])
         }).catch(() => message.error('Something went wrong :('))
     }
 
     const deleteVisit = (visit) => {
         SmartRequest.delete(`managment/doctorvisit/${visit.id}`).then(() => {
             setVisits(visits.filter((cur_doc) => cur_doc.id != visit.id))
-            message.success(`Deleted doctor ${visit.last_name[0]}. ${visit.first_name}`)
+            message.success(`Deleted visit ${visit.date.format('DD.MM.YY')} в ${visit.date.format('HH:mm')}`)
         })
     }
 
@@ -59,7 +71,7 @@ const Visits = () => {
         <Row justify='space-evenly' gutter={[2, 16]}>
             {visits.map(visit => (
                 <Col md={12} xl={8} xxl={6} key={visit.id}>
-                    <Card title={`Визит ${visit.date.format('DD.MM.YY')} ${visit.date.format('HH:mm:ss')}`}
+                    <Card title={`Визит ${visit.date.format('DD.MM.YY')} в ${visit.date.format('HH:mm')}`}
                         extra={
                             <>
                                 <Button type='link' danger onClick={() => deleteVisit(visit)} size='small'>
