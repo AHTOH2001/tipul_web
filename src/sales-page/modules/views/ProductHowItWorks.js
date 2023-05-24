530374b3a0c08dd0080236fdf9668d910a07fc2a
AHTOH2001 <Antonlapytko@gmail.com>
2023-05-24 09:06:24 +0300

    Added tariffs

--- src/sales-page/modules/views/ProductHowItWorks.js
+++ src/sales-page/modules/views/ProductHowItWorks.js
@@ -1,12 +1,13 @@
-import * as React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
+import mobileImage from '../../../images/sales/Smartphone-openslipart-ciubotaru.svg'
+import webImage from '../../../images/sales/mono_terminals_section.svg'
+import { SmartRequest } from '../../../utils/utils'
 import Button from '../components/Button'
 import Typography from '../components/Typography'
-import webImage from '../../../images/sales/mono_terminals_section.svg'
-import mobileImage from '../../../images/sales/Smartphone-openslipart-ciubotaru.svg'
 
 const item = {
     display: 'flex',
@@ -27,10 +28,33 @@ const image = {
     my: 4,
 }
 
+if (!String.prototype.format) {
+    String.prototype.format = function () {
+        var args = arguments
+        return this.replace(/{(\d+)}/g, function (match, number) {
+            return typeof args[number] != 'undefined'
+                ? args[number]
+                : match
+
+        })
+    }
+}
+
 function ProductHowItWorks() {
+
+    useEffect(() => {
+        SmartRequest.get('managment/tariff/')
+            .then(resp => {
+                setTariffs(resp.data)
+                console.log('HERE', resp.data)
+            })
+    }, [])
+
+    const [tariffs, setTariffs] = useState([])
+
     return (
         <Box
-            component="section"
+            component='section'
             sx={{
                 display: 'flex',
                 bgcolor: 'secondary.light',
@@ -48,9 +72,9 @@ function ProductHowItWorks() {
                 }}
             >
                 <Box
-                    component="img"
-                    src="https://mui.com/static/themes/onepirate/productCurvyLines.png"
-                    alt="curvy lines"
+                    component='img'
+                    src='https://mui.com/static/themes/onepirate/productCurvyLines.png'
+                    alt='curvy lines'
                     sx={{
                         pointerEvents: 'none',
                         position: 'absolute',
@@ -59,9 +83,9 @@ function ProductHowItWorks() {
                     }}
                 />
                 <Typography
-                    variant="h4"
-                    marked="center"
-                    component="h2"
+                    variant='h4'
+                    marked='center'
+                    component='h2'
                     sx={{ mb: 14 }}
                 >
                     Как это работает
@@ -72,15 +96,15 @@ function ProductHowItWorks() {
                             <Box sx={item}>
                                 <Box sx={number}>1.</Box>
                                 <Box
-                                    component="img"
+                                    component='img'
                                     src={mobileImage}
-                                    alt="mobile"
+                                    alt='mobile'
                                     sx={image}
                                 />
-                                <Typography variant="h5" align="center" style={{ fontSize: 25, fontWeight: 'bold', textAlign: 'start' }}>
+                                <Typography variant='h5' align='center' style={{ fontSize: 25, fontWeight: 'bold', textAlign: 'start' }}>
                                     Мобильное приложение
                                 </Typography>
-                                <Typography variant="h5" align="center" style={{ fontSize: 20, textAlign: 'start' }}>
+                                <Typography variant='h5' align='center' style={{ fontSize: 20, textAlign: 'start' }}>
                                     Позволяет вашим подопечным вести учёт приёма лекарств, врачей и визиты к ним самостоятельно.
                                     Разработан удобный и простой интерфейс, подходящий для слабовидящих людей.
                                 </Typography>
@@ -90,15 +114,15 @@ function ProductHowItWorks() {
                             <Box sx={item}>
                                 <Box sx={number}>2.</Box>
                                 <Box
-                                    component="img"
+                                    component='img'
                                     src={webImage}
-                                    alt="web"
+                                    alt='web'
                                     sx={image}
                                 />
-                                <Typography variant="h5" align="center" style={{ fontSize: 25, fontWeight: 'bold', textAlign: 'start' }}>
+                                <Typography variant='h5' align='center' style={{ fontSize: 25, fontWeight: 'bold', textAlign: 'start' }}>
                                     Веб-приложение
                                 </Typography>
-                                <Typography variant="h5" align="center" style={{ fontSize: 20, textAlign: 'start' }}>
+                                <Typography variant='h5' align='center' style={{ fontSize: 20, textAlign: 'start' }}>
                                     Позволяет Вам как опекуну контролировать процесс приёма лекарств и корректировать его.
                                     Вы сможете получать подробные отчёты о деятельности вашего подопечного.
                                 </Typography>
@@ -108,31 +132,32 @@ function ProductHowItWorks() {
                             <Box sx={item}>
                                 <Box sx={number}>3.</Box>
                                 <Box
-                                    component="img"
-                                    // src="https://mui.com/static/themes/onepirate/productHowItWorks3.svg"
-                                    src="https://mui.com/static/themes/onepirate/productValues3.svg"
-                                    alt="money"
+                                    component='img'
+                                    // src='https://mui.com/static/themes/onepirate/productHowItWorks3.svg'
+                                    src='https://mui.com/static/themes/onepirate/productValues3.svg'
+                                    alt='money'
                                     sx={image}
                                 />
-                                <Typography variant="h5" align="center" style={{ fontSize: 25, fontWeight: 'bold', textAlign: 'start' }}>
+                                <Typography variant='h5' align='center' style={{ fontSize: 25, fontWeight: 'bold', textAlign: 'start' }}>
                                     Тарифы
                                 </Typography>
-                                <Typography variant="h5" align="center" style={{ fontSize: 20, textAlign: 'start' }}>
-                                    Годовая подписка 32 рубля на веб-приложение.
-                                </Typography>
-                                <Typography variant="h5" align="center" style={{ fontSize: 20, textAlign: 'start' }}>
-                                    Годовая подписка 31 рубль на мобильное приложение.
-                                </Typography>
+                                {
+                                    tariffs.map(tariff => (
+                                        <Typography key={tariff.id} variant='h5' align='center' style={{ fontSize: 20, textAlign: 'start' }}>
+                                            {tariff.title.format(tariff.price.toString().slice(0, 2))}
+                                        </Typography>
+                                    ))
+                                }
                             </Box>
                         </Grid>
                     </Grid>
                 </div>
                 <Button
-                    color="secondary"
-                    size="large"
-                    variant="contained"
-                    component="a"
-                    href="/sign-up"
+                    color='secondary'
+                    size='large'
+                    variant='contained'
+                    component='a'
+                    href='/sign-up'
                     sx={{ mt: 8 }}
                 >
                     И это ещё не всё
